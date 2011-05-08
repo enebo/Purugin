7f80eab0a654555a4cdb05123fddba710eac6d3b
Thomas E. Enebo <tom.enebo@gmail.com>
2011-05-07 20:13:04 -0500

    Simple module for block actions

--- src/purugin.rb
+++ src/purugin.rb
@@ -81,6 +81,33 @@ module Purugin
       self.type == MATERIAL_NAMES[value]
     end  
   end
+  
+  module Action
+    A = org.bukkit.event.block.Action
+    def left_click_block?
+      getAction == A::LEFT_CLICK_BLOCK
+    end
+    
+    def left_click_air?
+      getAction == A::LEFT_CLICK_AIR
+    end
+    
+    def right_click_block?
+      getAction == A::RIGHT_CLICK_BLOCK
+    end
+    
+    def right_click_air?
+      getAction == A::RIGHT_CLICK_AIR
+    end
+    
+    def physical?
+      getAction == A::PHYSICAL
+    end
+  end
+end
+
+class org::bukkit::event::player::PlayerInteractEvent
+  include Purugin::Action
 end
 
 module org::bukkit::block::Block
