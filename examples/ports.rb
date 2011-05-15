ce8489123339a177157c017fbe366a2707604096
Thomas E. Enebo <tom.enebo@gmail.com>
2011-05-15 15:15:34 -0400

    Make larger set of acceptable teleport coords by base 36 encoding and shifting by 32k to get rid of minus signs

--- examples/ports.rb
+++ examples/ports.rb
@@ -1,8 +1,8 @@
 # Allow placed signs to behave as a teleporter when right clicked on.  The format
 # for the sign is line one containing 'Teleporter' and line two containing either:
-# a) x, y, z b) {locs_plus_waypoint_name}. In the case of b it will substitute
-# that name with the loc it represents when the sign is placed. You can write anything
-# on lines 3 or lower and it will not affect how the sign works as a teleporter.
+# a) x, y, z (base 36 offset by +32000) b) {locs_plus_waypoint_name}. In the case of b it 
+# will substitute that name with the loc it represents when the sign is placed. You can write
+# anything on lines 3 or lower and it will not affect how the sign works as a teleporter.
 class PortsPlugin
   include Purugin::Plugin
   description 'Ports', 0.2
@@ -12,9 +12,16 @@ class PortsPlugin
     state.get_line(1)
   end
   
-  VALUE = '-?\d+(?:\.\d+)?'
-  DEL = '\s*,\s*'
-  LOC = Regexp.new "\s*(#{VALUE})#{DEL}(#{VALUE})#{DEL}(#{VALUE})\s*"
+  VALUE = '[0-9a-z]+'
+  LOC = Regexp.new "(#{VALUE}),(#{VALUE}),(#{VALUE})"
+  
+  def encode(value)
+    (value.to_i + 32000).to_s(36)
+  end
+  
+  def decode(value)
+    value.to_i(36) - 32000
+  end
 
   def on_enable
     event(:player_interact) do |e|
@@ -23,8 +30,8 @@ class PortsPlugin
         return unless loc 
         
         if loc =~ LOC
-          x, y, z = $1.to_f, $2.to_f, $3.to_f
-          destination = org.bukkit.Location.new e.player.world, x, y, z
+          x, y, z = $1, $2, $3
+          destination = org.bukkit.Location.new e.player.world, decode(x), decode(y), decode(z)
           server.scheduler.schedule_sync_delayed_task(plugin) { e.player.teleport destination }
         end
       end
@@ -36,14 +43,16 @@ class PortsPlugin
     event(:sign_change) do |e|
       loc = teleporter_loc e
       return unless loc
-      return unless loc =~ /\{([^}]+)\}/
-      waypoint = $1
+      if loc =~ /\{([^}]+)\}/
+        waypoint = $1
       
-      locs_plus = plugin_manager['LocsPlus']
-      return unless locs_plus
+        locs_plus = plugin_manager['LocsPlus']
+        return unless locs_plus
 
-      waypoint, loc = *locs_plus.locations(e.player).find { |name, *| name == waypoint }
-      e.set_line 1, loc.map {|l| format "%0.1f", l }.join(",") if loc
+        waypoint, loc = *locs_plus.locations(e.player).find { |name, *| name == waypoint }
+      end
+      
+      e.set_line 1, loc[0..2].map {|l| encode(l) }.join(",") if loc
     end
   end
 end
