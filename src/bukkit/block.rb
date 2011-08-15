e236bf29e42981675eca5bca1157fae2a45494d6
Thomas E. Enebo <tom.enebo@gmail.com>
2011-08-15 17:54:48 -0500

    Make return nil if wrong face value used. Add rdoc

--- src/bukkit/block.rb
+++ src/bukkit/block.rb
@@ -11,9 +11,18 @@ module org::bukkit::block::Block
     :south_west => BF::SOUTH_WEST, :se => BF::SOUTH_EAST, :sw => BF::SOUTH_WEST, :self => BF::SELF
   }
   
+  # Find a nearby block relative to the current block based on a friendly symbol name
+  # === Parameters
+  # * _face_value_* can be :north, :east, :south, :west, :up, :down, :north_east, :north_west,
+  #      :ne, :nw, :south_east, :south_west, :se, :sw, :self
+  # === Examples
+  # here.block_at(:north) #=> another earth block
   def block_at(face_value)
     face = face_for(face_value)
-    get_face(face)
+    
+    return nil unless face
+    
+    get_relative(face)
   end
   
   def face_for(value)
