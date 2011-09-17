a3909e1a4c1bfdf5d8396ffe581ac931d5428ead
Thomas E. Enebo <tom.enebo@gmail.com>
2011-09-17 15:34:50 -0500

    Add block_at for world for absolute block retrieval

--- src/bukkit/world.rb
+++ src/bukkit/world.rb
@@ -1,6 +1,18 @@
 require 'java'
 
 module org::bukkit::World
+  # Get the block at the given coordinates
+  # === Parameters
+  # * x,y,z - Give three coord. location
+  # * location - Provide a location object
+  # === Example
+  # e.player.world.block_at(20, 20, 20) #=> Block instance
+  # e.player.world.block_at(Location.new(20, 20, 20)) #=> ditto
+  #
+  def block_at(*r)
+    getBlockAt *r
+  end
+  
   # Spawn a monster by name mob_name ("chicken", "creeper")
   # in the specified location
   def spawn_mob(mob_name, location)
