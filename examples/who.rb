72fae3ca81b364ff9577a3568e30fa922bb656d2
Thomas E. Enebo <tom.enebo@gmail.com>
2011-09-17 15:35:40 -0500

    Craftbukkit now has a time command for ops...remove admin and make who it's own plugin

--- examples/who.rb
+++ examples/who.rb
@@ -1,13 +1,10 @@
 class AdminPlugin
   include Purugin::Plugin
-  description 'Admin', 0.2
+  description 'Who', 0.2
   required :Commands, :include => :Command
   
   def on_enable
-    command('/time', 'change time +n hours') do |e, *args| 
-      e.player.world.time = args[1].to_i
-    end
-    command('/who', 'display all users') do |e, *args|
+    public_command('/who', 'display all users') do |e, *|
       me = e.player
       me.world.players.each do |player|
         me.send_message "#{player.display_name} (#{player.name})"
