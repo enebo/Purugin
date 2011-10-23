e21c58554f25beb97c2fa5d8a35fb013d412cb9c
Thomas E. Enebo <tom.enebo@gmail.com>
2011-10-23 09:01:31 -0500

    Add item? and block? methods to PlayerIteractEvent since we don't magically make those in JRuby from hasXXX methods

--- src/bukkit/event/player.rb
+++ src/bukkit/event/player.rb
@@ -2,5 +2,13 @@ require 'purugin/action'
 
 class org::bukkit::event::player::PlayerInteractEvent
   include Purugin::Action
+  
+  def block?
+    has_block
+  end
+  
+  def item?
+    has_item
+  end
 end
 
