f48e506cbe8f01804cfa9a7ccd95de8830686ce2
Thomas E. Enebo <tom.enebo@gmail.com>
2011-11-15 18:56:10 -0600

    Add dynamic predicates for all world environment types (e.g. nether? normal? skylands?...) and also an is?(env_type_str) for when you are unsure which environments exist (think custom mod envs which may or may not exist)

--- src/bukkit/world.rb
+++ src/bukkit/world.rb
@@ -31,4 +31,24 @@ module org::bukkit::World
   def storming?
     has_storm
   end
+  
+  # Is this world the type you think it is?
+  # === Parameters
+  # * environment_name is a str or symbol mapping to world you are checking against.
+  # === Example
+  # me.world.is? :nether
+  # === Notes
+  # Consider also using the direct env methods if you know for sure that a world environment
+  # exists on your server:
+  # me.world.nether?
+  def is?(environment_name)
+    environment_name.to_s.upcase == environment.name
+  end
+
+  # Define one method for each environment (e.g. nether?, normal? skylands?)
+  org.bukkit.World::Environment.values.each do |environment|
+    define_method(environment.name.downcase + "?") do
+      self.environment == environment
+    end
+  end
 end
