9129abb4e6d9a0688bfcbc95951b26bfaa80db5b
Marv Cool <marv@hostin.is>
2011-11-29 10:51:38 +0100

    tunnels can now be created with torches

--- examples/generators/tunnel.rb
+++ examples/generators/tunnel.rb
@@ -8,7 +8,8 @@ class TunnelGenerationPlugin
       depth = error? args[0].to_i, "Depth must be an integer value"
       height = error? args[1].to_i, "Height must be an integer value"
       width = error? args[2].to_i, "Width must be an integer value"
-      torches = args.length > 3 ? args[3] : false
+      torches = args[3].to_i == 1 ? true : false
+      breakit = args[4].to_i == 1 ? true : false
 
       error? depth > 0 || height > 0 || width > 0, "Dims must be integers >0"
       me.msg "Creating tunnel #{depth}x#{height}x#{width}"
@@ -35,8 +36,20 @@ class TunnelGenerationPlugin
         height.times do |height_dist|
           block = deep_block.block_at(height_facing,height_dist)
           width.times do |width_dist|
-            # for now the block will only be replaced by air. No mining.
-            block.block_at(width_facing,width_dist).break!
+            # The block will be mined (eg there will be a ItemStack with 
+            # its Material dropped) and changed to :air or to :torch
+            block.block_at(width_facing,width_dist).break! if breakit
+            
+            if torches and (depth_dist%3)==0 and (width_dist==0 or width_dist==width-1) and height_dist==height-1
+              # torched will be, however, not placed correctly but dropped on the ground.
+              # this seems to be something with the south/west whatsoever rule...
+              # If digging a tunnel to the west, everything is fine!
+              block.block_at(width_facing,width_dist).change_type :torch
+            else
+              # we could check if the block allready is :air but I dont care
+              block.block_at(width_facing,width_dist).change_type :air
+            end
+
           end
         end
       end
