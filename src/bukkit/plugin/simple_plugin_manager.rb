6850a572898af97e009acdd744a1727523983e37
Thomas E. Enebo <tom.enebo@gmail.com>
2012-01-26 09:48:12 -0600

    Temporarily disable listener unregistration until I can figure out a new way to do it

--- src/bukkit/plugin/simple_plugin_manager.rb
+++ src/bukkit/plugin/simple_plugin_manager.rb
@@ -4,7 +4,7 @@ class org::bukkit::plugin::SimplePluginManager
   # Hack: No exposure to commandMap and I don't want to hack in new plugin manager type
   field_reader :commandMap => :command_map
   
-  field_reader :listeners
+#  field_reader :listeners
   
   # We either register a new command of infect the existing one with the
   # new commands state if the command has already been registered.
@@ -19,12 +19,12 @@ class org::bukkit::plugin::SimplePluginManager
 
   # Hack to unregister all events for a particular plugin
   def unregister_events_for(plugin)
-    listeners.values.each do |type_set|
-      type_set.to_a.each do |item|
-        if item.plugin == plugin
-          type_set.remove item 
-        end
-      end
-    end
+#    listeners.values.each do |type_set|
+#      type_set.to_a.each do |item|
+#        if item.plugin == plugin
+#          type_set.remove item 
+#        end
+#      end
+#    end
   end
 end
