6744eacdf35ee06d86e26e6c4b3e7fae89f22cdb
Kai-Michael Pruess <kai.pruess@gmail.com>
2012-01-27 09:46:47 +0100

    fix for SimplePluginManager#unregister_events_for (could be refactored)

--- src/bukkit/plugin/simple_plugin_manager.rb
+++ src/bukkit/plugin/simple_plugin_manager.rb
@@ -4,8 +4,6 @@ class org::bukkit::plugin::SimplePluginManager
   # Hack: No exposure to commandMap and I don't want to hack in new plugin manager type
   field_reader :commandMap => :command_map
   
-#  field_reader :listeners
-  
   # We either register a new command of infect the existing one with the
   # new commands state if the command has already been registered.
   def add_command(command, prefix="purugin")
@@ -17,14 +15,8 @@ class org::bukkit::plugin::SimplePluginManager
     end
   end
 
-  # Hack to unregister all events for a particular plugin
+  # unregister all events for a particular plugin
   def unregister_events_for(plugin)
-#    listeners.values.each do |type_set|
-#      type_set.to_a.each do |item|
-#        if item.plugin == plugin
-#          type_set.remove item 
-#        end
-#      end
-#    end
+    org.bukkit.event.HandlerList.unregisterAll plugin
   end
 end
