f1495ae212f8cf9d947f3b9b46e8f62ae2fc0c3b
Thomas E. Enebo <tom.enebo@gmail.com>
2012-01-31 09:46:21 -0600

    rdoc

--- src/purugin/materials.rb
+++ src/purugin/materials.rb
@@ -1,9 +1,11 @@
 require 'java'
 
 module Purugin
+  ##
   # Convenience methods useful for dealing with anything which works with Materials.  Block is
   # one type which includes this.
   module Materials
+    ##
     # Is this block any of the supplied values (see MATERIAL_NAMES constant for a full list)?
     # === Parameters
     # * _values_ one or more symbols from MATERIAL_NAMES that you want to test against
@@ -27,11 +29,13 @@ module Purugin
       respond_to? :isPowered ? isPowered : false
     end
     
+    ##
     # Return the Java equivalent value for the specified material symbol/str
     def material_for(value)
       org.bukkit.Material.match_material value.to_s
     end
     
+    ##
     # Is this block/material solid enough to walk on (or possibly attach to)
     # === Example
     # stone_block.solid? #=> true
