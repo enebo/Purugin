6e3c5227dabf3089c1db62513aa8377fc7825bd3
Thomas E. Enebo <tom.enebo@gmail.com>
2012-05-16 09:35:49 -0500

    Make arg processing work.  Add some more comments for example.  Move block directive to top since it is always drawing with the same type

--- examples/purogo/cube.rb
+++ examples/purogo/cube.rb
@@ -1,36 +1,38 @@
 turtle("cube", :pig) do |*args|
-if args[0] >= 1
-  length = 5
-else
-  length = args[0].to_i
-end
-if args[1] == nil
-  block_type = :stone
-else
-  block_type = args[1].to_sym
-end
-  
+  if args.length >= 1
+    length = args[0].to_i
+  else
+    length = 5
+  end
+
+  if args.length >= 2
+    block_type = args[1].to_sym
+  else
+    block_type = :stone
+  end
+
+  # Set block to be used for all drawing
+  block block_type  
+
   # Draw base of cube
   4.times do |i|
     mark i # Mark 4 lower corners
-    block block_type
     forward length
     turnleft 90
   end
 
   # Draw one up pillar and save top of cube
   4.times do |i|
-    block block_type
     goto i 
     turnup 90
     forward length
   end # Still at top of last pillar
 
+  # Still pointing straight up after last draw turn back horizontal
   turndown 90
 
   # Draw top of cube
   4.times do |i|
-    block block_type
     forward length
     turnleft 90
   end
