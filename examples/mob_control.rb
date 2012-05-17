a34ddff34379b364ffe7802628a5f783e9341a85
Thomas E. Enebo <tom.enebo@gmail.com>
2012-05-17 14:44:02 -0500

    HEH...fix it

--- examples/mob_control.rb
+++ examples/mob_control.rb
@@ -3,25 +3,36 @@ class MobControlPlugin
 
   include Purugin::Plugin, Purugin::Colors
   description 'control world mob spawning', 0.1
+
+  def get_values
+    return config.get!("mob_control.allow_monsters", true), config.get!("mob_control.allow_animals", true)
+  end
+
+  def display_values(me)
+    allow_monsters, allow_animals = get_values
+    me.msg green("MONSTERS: #{allow_monsters}, ANIMALS: #{allow_animals}")
+  end
   
   def on_enable
-    allow_monsters = config.get "mob_control.allow_monsters", true
-    allow_animals = config.get "mob_control.allow_animals", true
+    allow_monsters, allow_animals = get_values
 
     server.worlds.each do |world| 
       world.set_spawn_flags(allow_monsters, allow_animals)
     end
     
-    public_command('mob_control', 'Can mobs spawn', '/mob_control mons ani') do |me, *args|
-      allow_monsters = TRUTH.include?(args[0]) if args[0]
-      allow_animals = TRUTH.include?(args[1]) if args[1]
+    public_command('mob_control', 'Can mobs spawn', '/mob_control mons anim') do |me, *args|
+      if args.length == 0
+        display_values(me)
+      else
+        allow_monsters = TRUTH.include?(args[0]) if args[0]
+        allow_animals = TRUTH.include?(args[1]) if args[1]
 
-      config.set! "mob_control.allow_monsters", allow_monsters
-      config.set! "mob_control.allow_monsters", allow_animals
+        config.set! "mob_control.allow_monsters", allow_monsters
+        config.set! "mob_control.allow_animals", allow_animals
+        me.world.set_spawn_flags(allow_monsters, allow_animals)
 
-      me.msg green("MONSTERS: #{allow_monsters}, ANIMALS: #{allow_animals}")
-
-      me.world.set_spawn_flags(allow_monsters, allow_animals)
+        display_values(me)
+      end
     end
   end
 end
