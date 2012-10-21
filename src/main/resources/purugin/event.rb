258d2a8447f479fc755d27728d6ead2c3cc2e155
Thomas E. Enebo <tom.enebo@gmail.com>
2012-10-21 12:06:30 -0500

    make EVENT_NAME_TO_LISTENER part of define_event_listener

--- src/main/resources/purugin/event.rb
+++ src/main/resources/purugin/event.rb
@@ -1,8 +1,13 @@
 require 'java'
+require 'purugin/util'
 
 module Purugin
   module EventDefiner
+    include Purugin::StringUtils
+
+    # FIXME: Add list of priorities as optional param
     def define_event_listener(name, package)
+      event_name = camelcase_to(name.sub(/Event$/, ''), '_').downcase
       self.module_eval <<-EOS
         class #{name}Listener
           include org.bukkit.event.Listener
@@ -11,10 +16,12 @@ module Purugin
           add_method_signature 'on_event', [java.lang.Void::TYPE, #{package}.#{name}]
           add_method_annotation 'on_event', org.bukkit.event.EventHandler => {}
           become_java!
-        end      
+        end
+        Purugin::Event::EVENT_NAME_TO_LISTENER[:#{event_name}] = #{name}Listener
       EOS
     end    
   end
+  
   module Event
     # FIXME: This only handles normal priority.  Real fix is to generate these on demand versus up
     # front.  We can reify what we need.  This will also fix custom event types.
@@ -23,8 +30,12 @@ module Purugin
     
     extend Purugin::EventDefiner
     
+    ##
+    # symbol -> listener class map populated by calls to define_event_listener
+    EVENT_NAME_TO_LISTENER = {}     
+    
     # These listeners and the map below come from auto-generation via generate_ruby_listeners_hack    
-     define_event_listener('AsyncPlayerChatEvent', 'org.bukkit.event.player')
+    define_event_listener('AsyncPlayerChatEvent', 'org.bukkit.event.player')
     define_event_listener('AsyncPlayerPreLoginEvent', 'org.bukkit.event.player')
     define_event_listener('BlockBreakEvent', 'org.bukkit.event.block')
     define_event_listener('BlockBurnEvent', 'org.bukkit.event.block')
@@ -177,162 +188,6 @@ module Purugin
     define_event_listener('WorldLoadEvent', 'org.bukkit.event.world')
     define_event_listener('WorldSaveEvent', 'org.bukkit.event.world')
     define_event_listener('WorldUnloadEvent', 'org.bukkit.event.world')
-
-    EVENT_NAME_TO_LISTENER = {
-      :async_player_chat => AsyncPlayerChatEventListener,
-      :async_player_pre_login => AsyncPlayerPreLoginEventListener,
-      :block_break => BlockBreakEventListener,
-      :block_burn => BlockBurnEventListener,
-      :block_can_build => BlockCanBuildEventListener,
-      :block_damage => BlockDamageEventListener,
-      :block_dispense => BlockDispenseEventListener,
-      :block => BlockEventListener,
-      :block_fade => BlockFadeEventListener,
-      :block_form => BlockFormEventListener,
-      :block_from_to => BlockFromToEventListener,
-      :block_grow => BlockGrowEventListener,
-      :block_ignite => BlockIgniteEventListener,
-      :block_physics => BlockPhysicsEventListener,
-      :block_piston => BlockPistonEventListener,
-      :block_piston_extend => BlockPistonExtendEventListener,
-      :block_piston_retract => BlockPistonRetractEventListener,
-      :block_place => BlockPlaceEventListener,
-      :block_redstone => BlockRedstoneEventListener,
-      :block_spread => BlockSpreadEventListener,
-      :brew => BrewEventListener,
-      :chunk => ChunkEventListener,
-      :chunk_load => ChunkLoadEventListener,
-      :chunk_populate => ChunkPopulateEventListener,
-      :chunk_unload => ChunkUnloadEventListener,
-      :conversation_abandoned => ConversationAbandonedEventListener,
-      :craft_item => CraftItemEventListener,
-      :creature_spawn => CreatureSpawnEventListener,
-      :creeper_power => CreeperPowerEventListener,
-      :enchant_item => EnchantItemEventListener,
-      :entity_block_form => EntityBlockFormEventListener,
-      :entity_break_door => EntityBreakDoorEventListener,
-      :entity_change_block => EntityChangeBlockEventListener,
-      :entity_combust_by_block => EntityCombustByBlockEventListener,
-      :entity_combust_by_entity => EntityCombustByEntityEventListener,
-      :entity_combust => EntityCombustEventListener,
-      :entity_create_portal => EntityCreatePortalEventListener,
-      :entity_damage_by_block => EntityDamageByBlockEventListener,
-      :entity_damage_by_entity => EntityDamageByEntityEventListener,
-      :entity_damage => EntityDamageEventListener,
-      :entity_death => EntityDeathEventListener,
-      :entity => EntityEventListener,
-      :entity_explode => EntityExplodeEventListener,
-      :entity_interact => EntityInteractEventListener,
-      :entity_portal_enter => EntityPortalEnterEventListener,
-      :entity_regain_health => EntityRegainHealthEventListener,
-      :entity_shoot_bow => EntityShootBowEventListener,
-      :entity_tame => EntityTameEventListener,
-      :entity_target => EntityTargetEventListener,
-      :entity_target_living_entity => EntityTargetLivingEntityEventListener,
-      :entity_teleport => EntityTeleportEventListener,
-      :exp_bottle => ExpBottleEventListener,
-      :explosion_prime => ExplosionPrimeEventListener,
-      :food_level_change => FoodLevelChangeEventListener,
-      :furnace_burn => FurnaceBurnEventListener,
-      :furnace_smelt => FurnaceSmeltEventListener,
-      :inventory_click => InventoryClickEventListener,
-      :inventory_close => InventoryCloseEventListener,
-      :inventory => InventoryEventListener,
-      :inventory_open => InventoryOpenEventListener,
-      :item_despawn => ItemDespawnEventListener,
-      :item_spawn => ItemSpawnEventListener,
-      :leaves_decay => LeavesDecayEventListener,
-      :lightning_strike => LightningStrikeEventListener,
-      :map_initialize => MapInitializeEventListener,
-      :note_play => NotePlayEventListener,
-      :painting_break_by_entity => PaintingBreakByEntityEventListener,
-      :painting_break => PaintingBreakEventListener,
-      :painting => PaintingEventListener,
-      :painting_place => PaintingPlaceEventListener,
-      :pig_zap => PigZapEventListener,
-      :player_animation => PlayerAnimationEventListener,
-      :player_bed_enter => PlayerBedEnterEventListener,
-      :player_bed_leave => PlayerBedLeaveEventListener,
-      :player_bucket_empty => PlayerBucketEmptyEventListener,
-      :player_bucket => PlayerBucketEventListener,
-      :player_bucket_fill => PlayerBucketFillEventListener,
-      :player_changed_world => PlayerChangedWorldEventListener,
-      :player_channel => PlayerChannelEventListener,
-      :player_chat => PlayerChatEventListener,
-      :player_chat_tab_complete => PlayerChatTabCompleteEventListener,
-      :player_command_preprocess => PlayerCommandPreprocessEventListener,
-      :player_death => PlayerDeathEventListener,
-      :player_drop_item => PlayerDropItemEventListener,
-      :player_egg_throw => PlayerEggThrowEventListener,
-      :player => PlayerEventListener,
-      :player_exp_change => PlayerExpChangeEventListener,
-      :player_fish => PlayerFishEventListener,
-      :player_game_mode_change => PlayerGameModeChangeEventListener,
-      :player_interact_entity => PlayerInteractEntityEventListener,
-      :player_interact => PlayerInteractEventListener,
-      :player_inventory => PlayerInventoryEventListener,
-      :player_item_break => PlayerItemBreakEventListener,
-      :player_item_held => PlayerItemHeldEventListener,
-      :player_join => PlayerJoinEventListener,
-      :player_kick => PlayerKickEventListener,
-      :player_level_change => PlayerLevelChangeEventListener,
-      :player_login => PlayerLoginEventListener,
-      :player_move => PlayerMoveEventListener,
-      :player_pickup_item => PlayerPickupItemEventListener,
-      :player_portal => PlayerPortalEventListener,
-      :player_pre_login => PlayerPreLoginEventListener,
-      :player_quit => PlayerQuitEventListener,
-      :player_register_channel => PlayerRegisterChannelEventListener,
-      :player_respawn => PlayerRespawnEventListener,
-      :player_shear_entity => PlayerShearEntityEventListener,
-      :player_teleport => PlayerTeleportEventListener,
-      :player_toggle_flight => PlayerToggleFlightEventListener,
-      :player_toggle_sneak => PlayerToggleSneakEventListener,
-      :player_toggle_sprint => PlayerToggleSprintEventListener,
-      :player_unregister_channel => PlayerUnregisterChannelEventListener,
-      :player_velocity => PlayerVelocityEventListener,
-      :plugin_disable => PluginDisableEventListener,
-      :plugin_enable => PluginEnableEventListener,
-      :plugin => PluginEventListener,
-      :portal_create => PortalCreateEventListener,
-      :potion_splash => PotionSplashEventListener,
-      :prepare_item_craft => PrepareItemCraftEventListener,
-      :prepare_item_enchant => PrepareItemEnchantEventListener,
-      :projectile_hit => ProjectileHitEventListener,
-      :projectile_launch => ProjectileLaunchEventListener,
-      :remote_server_command => RemoteServerCommandEventListener,
-      :server_command => ServerCommandEventListener,
-      :server => ServerEventListener,
-      :server_list_ping => ServerListPingEventListener,
-      :service => ServiceEventListener,
-      :service_register => ServiceRegisterEventListener,
-      :service_unregister => ServiceUnregisterEventListener,
-      :sheep_dye_wool => SheepDyeWoolEventListener,
-      :sheep_regrow_wool => SheepRegrowWoolEventListener,
-      :sign_change => SignChangeEventListener,
-      :slime_split => SlimeSplitEventListener,
-      :spawn_change => SpawnChangeEventListener,
-      :structure_grow => StructureGrowEventListener,
-      :thunder_change => ThunderChangeEventListener,
-      :vehicle_block_collision => VehicleBlockCollisionEventListener,
-      :vehicle_collision => VehicleCollisionEventListener,
-      :vehicle_create => VehicleCreateEventListener,
-      :vehicle_damage => VehicleDamageEventListener,
-      :vehicle_destroy => VehicleDestroyEventListener,
-      :vehicle_enter => VehicleEnterEventListener,
-      :vehicle_entity_collision => VehicleEntityCollisionEventListener,
-      :vehicle => VehicleEventListener,
-      :vehicle_exit => VehicleExitEventListener,
-      :vehicle_move => VehicleMoveEventListener,
-      :vehicle_update => VehicleUpdateEventListener,
-      :weather_change => WeatherChangeEventListener,
-      :weather => WeatherEventListener,
-      :world => WorldEventListener,
-      :world_init => WorldInitEventListener,
-      :world_load => WorldLoadEventListener,
-      :world_save => WorldSaveEventListener,
-      :world_unload => WorldUnloadEventListener,
-    }
     
     def event(event_name, priority_value=:lowest, &code)
       type = EVENT_NAME_TO_LISTENER[event_name.to_sym]
