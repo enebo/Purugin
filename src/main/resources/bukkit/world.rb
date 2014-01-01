e27b1bed42bf6574c74c07d09478c0dbc665fa01
Thomas E. Enebo <tom.enebo@gmail.com>
2014-01-01 16:02:47 -0600

    Add World#chunk_at and  World#highest_block_at and update some comments.

--- src/main/resources/bukkit/world.rb
+++ src/main/resources/bukkit/world.rb
@@ -7,9 +7,9 @@ module org::bukkit::World
   ##
   # Get the block at the given coordinates
   # === Parameters
-  # * x,y,z - Give three coord. location
-  # * location - Provide a location object
-  # === Example
+  # * _x_,_y_,_z_ - Give three coord. location
+  # * _location_ - Provide a location object
+  # === Examples
   # e.player.world.block_at(20, 20, 20) #=> Block instance
   # e.player.world.block_at(Location.new(20, 20, 20)) #=> ditto
   #
@@ -18,10 +18,40 @@ module org::bukkit::World
   end
   
   ##
+  # Gets the chunk that this location is within.
+  # === Parameters
+  # * _location_ - location within the chunk you are asking for
+  # === Examples
+  # e.player.world.chunk_at(me)  #=> Location coerced from your location
+  # e.player.world.chunk_at(some_location) #=> Give an explicit location
+  def chunk_at(location)
+    location = location.respond_to?(:to_loc) ? location.to_loc : location
+    
+    getChunkAt(location)
+  end
+  
+  ##
+  # Get the highest non-air block in the y dimension of the supplied locations x,z
+  # coordinate.
+  # === Parameters
+  # * _location_ - x,z location you want highest y non-air block
+  # === Examples
+  # e.player.world.highest_block_at(me)  #=> Location coerced from your location
+  # e.player.world.highest_block_at(some_location) #=> Give an explicit location
+  #
+  def highest_block_at(location)
+    location = location.respond_to?(:to_loc) ? location.to_loc : location
+    
+    getHighestBlockAt location
+  end
+  
+  ##
   # Spawn a creature ("chicken", "creeper") at a specified location.
   # === Parameters
-  # * mob_name is the creature to spawn (@see CreatureType)
-  # * location is where to put the spawned creature [note: to_loc coercion possible]
+  # * _mob_name_ is the creature to spawn (@see CreatureType)
+  # * _location_ is where to put the spawned creature [note: to_loc coercion possible]
+  # === Examples
+  # e.player.world.spawn_mob(:chicken, e.player.location)
   #
   def spawn_mob(creature_name, location)
     creature_type = org.bukkit.entity.EntityType.from_name(creature_name.to_s)
