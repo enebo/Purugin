ca6210b6dd9c944273679411f7c38d233ba14b0d
Thomas E. Enebo <tom.enebo@gmail.com>
2014-01-27 17:46:12 -0600

    Add some more world methods + docs and also convert world to be its concrete type

--- src/main/resources/bukkit/world.rb
+++ src/main/resources/bukkit/world.rb
@@ -1,7 +1,7 @@
 require 'java'
 require 'purugin/predicate'
 
-module org::bukkit::World
+class cb::CraftWorld
   extend Purugin::Predicate
   
   ##
@@ -28,6 +28,86 @@ module org::bukkit::World
     location = location.respond_to?(:to_loc) ? location.to_loc : location
     
     getChunkAt(location)
+  end
+  
+  ##
+  # Is the provided chunk currently loaded by this world.  This also can be called
+  # with the
+  # === Parameters
+  # * _chunk_or_x_ * - is the chunk instance you are inquiring about or it is x coordinate
+  # * z * - is optional for the [x,y] version of this method.
+  # === Examples
+  # e.player.world.chunk_loaded? some_chunk
+  # e.player.world.chunk_loaded?(30, 13)
+  def chunk_loaded?(chunk_or_x, z=nil)
+    z ? isChunkLoaded(chunk_or_x, z) : isChunkLoaded(chunk_or_x)
+  end
+  
+  ##
+  # Returns an Array of loaded chunks (Java version returns Java primitive array).
+  #
+  def loaded_chunks
+    getLoadedChunks.to_a
+  end
+
+  ##
+  # Is the chunk being actively used by players (also must be loaded).
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk
+  def chunk_in_use?(x, z)
+    isChunkInUse(x, z)
+  end  
+
+  ##
+  # Load the chunk at [x,z] and choose whether you want it generated or not (default: true).
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk
+  # _generate_ - should the chunk get generated if it hasn't before (default: true)
+  def load_chunk(x, z, generate=true)
+    loadChunk(x, z, generate)
+  end
+  
+  ##
+  # Unload the chunk at [x,z] and choose whether it should be saved away (_save_: true) and
+  # whether we should care if players are close to that chunk or not (_safe_: true).
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk
+  # _save_ - save the chunk to disk
+  # _safe_ - only unload it is no players are close enough to notice  
+  def unload_chunk(x, z, save=true, safe=true)
+    unloadChunk(x, z, save, safe)
+  end
+
+  ##
+  # Sends a request to specify you want to unload a chunk at [x,z].  Optionally, you can
+  # specify whether you want this operation to be safe or not (_safe_: true).  
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk
+  # _safe_ - only unload it is no players are close enough to notice    
+  def unload_chunk_request(x, z, safe=true)
+    unloadChunkRequest(x, z, safe)
+  end
+  
+  ##
+  # Regenerate the chunk at [_x_, _z_].
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk  
+  def regenerate_chunk(x, z)
+    regenerateChunk(x, z)  
+  end
+  
+  ##
+  # Refreshes the chunk at [_x_, _z_].
+  # === Parameters
+  # _x_ - x coordinate of chunk
+  # _z_ - z coordinate of chunk  
+  def refresh_chunk(x, z)
+    refreshChunk(x, z)
   end
   
   ##
